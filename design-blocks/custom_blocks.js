7c57a07f626958ab37b73dbe413988a171859bfc
Kaitlyn Vetica <78003136+kvetica@users.noreply.github.com>
2023-09-24 19:40:30 -0700

    Added arguments with each block

--- design-blocks/custom_blocks.js
+++ design-blocks/custom_blocks.js
@@ -3,6 +3,9 @@ Blockly.Blocks['add_course'] = {
     init: function() {
         this.appendDummyInput()
             .appendField("Add Course");
+        this.appendValueInput("ARGUMENT")
+            .setCheck(null)
+            .appendField("with data:");
         this.setColour(230);
         this.setTooltip("Add a new course to the database");
         this.setHelpUrl("");
@@ -14,6 +17,11 @@ Blockly.Blocks['find_course'] = {
     init: function() {
         this.appendDummyInput()
             .appendField("Find Course");
+        this.appendValueInput("ARGUMENT")
+            .setCheck(null)
+            .appendField("where")
+            .appendField(new Blockly.FieldTextInput("code"), "CRITERIA")
+            .appendField("=");
         this.setColour(230);
         this.setTooltip("Find information about a course");
         this.setHelpUrl("");
@@ -25,6 +33,11 @@ Blockly.Blocks['update_course'] = {
     init: function() {
         this.appendDummyInput()
             .appendField("Update Course");
+        this.appendValueInput("ARGUMENT")
+            .setCheck(null)
+            .appendField("where")
+            .appendField(new Blockly.FieldTextInput("code"), "CRITERIA")
+            .appendField("with data:");
         this.setColour(230);
         this.setTooltip("Update course information");
         this.setHelpUrl("");
@@ -36,6 +49,8 @@ Blockly.Blocks['delete_course'] = {
     init: function() {
         this.appendDummyInput()
             .appendField("Delete Course");
+        this.appendValueInput("ARGUMENT")
+            .setCheck(null);
         this.setColour(230);
         this.setTooltip("Delete a course from the database");
         this.setHelpUrl("");
@@ -47,6 +62,8 @@ Blockly.Blocks['generate_plan'] = {
     init: function() {
         this.appendDummyInput()
             .appendField("Generate Plan");
+        this.appendValueInput("ARGUMENT")
+            .setCheck(null);
         this.setColour(230);
         this.setTooltip("Generate a four-year plan");
         this.setHelpUrl("");
